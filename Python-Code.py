2c72c3c320254821e2b9d7f9079089c473aaf5e3
Saeed <102333571+saeedbd21@users.noreply.github.com>
2023-04-12 01:11:36 +0100

    Python code using the same api and key

---
+++ Python-Code.py
@@ -0,0 +1,36 @@
+import openai
+import re
+
+# set up OpenAI API key
+openai.api_key = "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"
+
+# define function to generate questions
+def generate_questions(paragraph, num_questions=3):
+    # split paragraph into sentences
+    sentences = re.split('(?<=\w\.)\s', paragraph)
+
+    # initialize list to store generated questions
+    questions = []
+
+    # loop through sentences and generate questions using ChatGPT
+    for sentence in sentences:
+        for i in range(num_questions):
+            prompt = "Generate a question for this sentence: " + sentence
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=50,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+            question = response.choices[0].text.strip()
+            questions.append(question)
+
+    # return list of generated questions
+    return questions
+
+# example usage
+paragraph = "The key difference between a Docker image Vs a container is that a Docker image is a read-only immutable template that defines how a container will be realized. A Docker container is a runtime instance of a Docker image that gets created when the $ docker run command is implemented."
+questions = generate_questions(paragraph, num_questions=5)
+print(questions)
